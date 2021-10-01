e79eef6cb24c286cdeac142645f7177864c931b4
Reazour Rahman <alexrabbi111@gmail.com>
2021-10-02 02:31:33 +0600

    routing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../../images/logo.png'
 import './Header.css'
 
@@ -12,9 +13,9 @@ const Header = (props) => {
             </div>
             <nav className="menu">
                 <div className="anchor-container">
-                <a href="/shop">Shop</a>
-                <a href="/review">Order Review</a>
-                <a href="/inventory">Manage Inventory Here</a>
+                <NavLink to="/shop">Shop</NavLink>
+                <NavLink to="/review">Order Review</NavLink>
+                <NavLink to="/inventory">Manage Inventory Here</NavLink>
                 </div>
             </nav>
 
